f50815251a34e01ef2c350aece5bb5eed4fabe65
unknown <11211087@student.itk.ac.id>
2023-10-23 22:41:06 +0700

    Buat SELECT Tabel

--- app/settings.php
+++ app/settings.php
@@ -23,7 +23,7 @@ return function (ContainerBuilder $containerBuilder) {
                 ],
                 'db' => [
                     'host' => '127.0.0.1',
-                    'dbname' => 'mbd-slim-api',
+                    'dbname' => 'mahasiswa_informatika',
                     'user' => 'root',
                     'password' => '',
                 ]
